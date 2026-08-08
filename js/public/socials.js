0524fee38ba80f925f675633f7634b152855e59a
tcisolutions <tcmcorreo@gmail.com>
2026-08-08 04:34:48 -0600

    Add files via upload

--- js/public/socials.js
+++ js/public/socials.js
@@ -1,55 +1,17 @@
 import { renderCard } from "../components/Card.js";
 
-export function renderSummary(portal){
+export function renderSocials(portal){
 
     return renderCard(
 
-        "Información del equipo",
+        "Síguenos",
 
         `
+        <div style="text-align:center;padding:20px">
 
-        <div class="info-grid">
-
-            <div>
-
-                <strong>Cliente</strong>
-
-                <span>${portal.order.client}</span>
-
-            </div>
-
-            <div>
-
-                <strong>Equipo</strong>
-
-                <span>
-
-                    ${portal.order.brand}
-
-                    ${portal.order.model}
-
-                </span>
-
-            </div>
-
-            <div>
-
-                <strong>IMEI</strong>
-
-                <span>${portal.order.imei || "-"}</span>
-
-            </div>
-
-            <div>
-
-                <strong>Falla</strong>
-
-                <span>${portal.order.issue}</span>
-
-            </div>
+            Próximamente...
 
         </div>
-
         `
 
     );
